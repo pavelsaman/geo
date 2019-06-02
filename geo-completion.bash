1e0051e379a2dbb2b851e7545c049b9c5f4c51b6
pavelsam <pavelsam@centrum.cz>
2019-06-02 20:55:25 +0200

    Autocomplete of countries

--- geo-completion.bash
+++ geo-completion.bash
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+countries_file="${HOME}/.countries"
+countries=()
+
+function _suggest_countries {
+	if [[ -f $countries_file && -r $countries_file ]]; then
+		while read -r country; do
+			countries+=("$country")
+		done < "$countries_file"
+	fi
+}
+
+
 function _geo_completion {
 	if (( ${COMP_CWORD} == 1 )); then
 		COMPREPLY=($(compgen -W "-c -C --lat --lon" -- "${COMP_WORDS[1]}" 2>/dev/null))
@@ -14,6 +26,17 @@ function _geo_completion {
 			COMPREPLY=($(compgen -W "--lat" -- "${COMP_WORDS[3]}" 2>/dev/null))
 		else
 			return
+		fi
+	elif (( ${COMP_CWORD} == 2 )); then
+		_suggest_countries
+
+		local IFS=$'\n'
+		candidates=($(compgen -W "${countries[*]}" -- "${COMP_WORDS[2]}"))
+		
+		if (( ${#candidates[*]} != 0 )); then
+			COMPREPLY=($(printf '%q\n' "${candidates[@]}" 2>/dev/null))
+		else
+			COMPREPLY=()
 		fi
 	fi	
 }
